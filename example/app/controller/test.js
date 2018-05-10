38186f073fb0999be8c1f73dc16f617dd40ac1be
heqing <786699892@qq.com>
2018-05-10 18:34:00 +0800

    no message

--- example/app/controller/test.js
+++ example/app/controller/test.js
@@ -1,31 +1,31 @@
-module.exports = class extends qingful.controller {
-	async _initialize() {
-		// 控制器初始化
-		console.log("_initialize");
-	}
+module.exports = class extends qingful.Controller {
+    async _initialize() {
+        // 控制器初始化
+        console.log("_initialize");
+    }
 
-	async _before() {
-		// 控制器前置
-		console.log("_before");
-	}
+    async _before() {
+        // 控制器前置
+        console.log("_before");
+    }
 
-	async _before_index() {
-		// 方法前置
-		console.log("_before_index");
-	}
+    async _before_index() {
+        // 方法前置
+        console.log("_before_index");
+    }
 
-	async index() {
-		console.log("index");
-		this.success("Hello Qingful");
-	}
+    async index() {
+        console.log("index");
+        this.success("Hello Qingful");
+    }
 
-	async _after_index() {
-		// 方法后置
-		console.log("_after_index");
-	}
+    async _after_index() {
+        // 方法后置
+        console.log("_after_index");
+    }
 
-	async _after() {
-		// 控制器后置
-		console.log("_after");
-	}
+    async _after() {
+        // 控制器后置
+        console.log("_after");
+    }
 };

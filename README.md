475c60b5d69e330d75711123edec8830ee877cf5
heqing <786699892@qq.com>
2018-05-11 12:31:47 +0800

    no message

---
+++ README.md
@@ -0,0 +1,3 @@
+## Qingful.js
+
+Qingful.js -- 青否科技最佳实践Node.js Web快速开发框架。
--- example/app/controller/test.js
+++ example/app/controller/test.js
@@ -14,6 +14,12 @@ module.exports = class extends qingful.Controller {
         console.log("_before_index");
     }
 
+    /**
+     * Represents a book.
+     * @constructor
+     * @param {string} title - The title of the book.
+     * @param {string} author - The author of the book.
+     */
     async index() {
         console.log("index");
         this.success("Hello Qingful");
